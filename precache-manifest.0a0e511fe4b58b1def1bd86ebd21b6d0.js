dd1cf70127e47c8aff0cadad92cdc077f8250928
404UsernameNotFound404 <37249924+404UsernameNotFound404@users.noreply.github.com>
2019-07-26 23:13:24 -0400

    Updates

---
+++ precache-manifest.0a0e511fe4b58b1def1bd86ebd21b6d0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9b26819334a51f80a664c712e9ca812",
+    "url": "/react-gh-pages/static/media/ReactApp.b9b26819.png"
+  },
+  {
+    "revision": "6f33f87de5a58eb8c9bc",
+    "url": "/react-gh-pages/static/css/main.bfcc8e39.chunk.css"
+  },
+  {
+    "revision": "cde22863ffca018955eb",
+    "url": "/react-gh-pages/static/js/runtime~main.cde22863.js"
+  },
+  {
+    "revision": "58b6c76246c5a358e508",
+    "url": "/react-gh-pages/static/js/2.58b6c762.chunk.js"
+  },
+  {
+    "revision": "f6ffea09473468fcc5b4c34e8a192d18",
+    "url": "/react-gh-pages/static/media/0.f6ffea09.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "92559dbafc5e84cfcc19f5991aa78c67",
+    "url": "/react-gh-pages/static/media/JAGame.92559dba.png"
+  },
+  {
+    "revision": "6f33f87de5a58eb8c9bc",
+    "url": "/react-gh-pages/static/js/main.6f33f87d.chunk.js"
+  },
+  {
+    "revision": "30e016883e7496de4da4518a9ede2109",
+    "url": "/react-gh-pages/static/media/404UsernameNotFound404.30e01688.png"
+  },
+  {
+    "revision": "edec320b0786e7c1bbc81559801768d3",
+    "url": "/react-gh-pages/static/media/ProgrammerHumor.edec320b.png"
+  },
+  {
+    "revision": "89590f42f803c2cf04eae5de5637ccc6",
+    "url": "/react-gh-pages/static/media/ann.89590f42.jpg"
+  },
+  {
+    "revision": "94fd4be9ebec6183205b65ec076b7f36",
+    "url": "/react-gh-pages/static/media/properIntro.94fd4be9.png"
+  },
+  {
+    "revision": "9dac2672b4d35b9cafb380f0e79bafda",
+    "url": "/react-gh-pages/static/media/shoulderAn.9dac2672.jpg"
+  },
+  {
+    "revision": "6a8dcaecfb3d45a5def78a5175b5272a",
+    "url": "/react-gh-pages/static/media/AnnotomyOfShoulder.6a8dcaec.jpg"
+  },
+  {
+    "revision": "125950700c41ec13f1dbb3027e449fc7",
+    "url": "/react-gh-pages/index.html"
+  }
+];
